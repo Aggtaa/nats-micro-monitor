8ac6b0fa0d76e57e05b7515b8fedb690db19adac
Anton Zykov <a.zykov@optimacros.com>
2024-04-11 18:06:45 +0300

    removed http-gateway (moved to a separate repo)

--- apps/http-gateway/src/debug.ts
+++
@@ -1,9 +0,0 @@
-import * as debugThreadsNs from 'debug-threads-ns';
-
-type Debug = debugThreadsNs.ExtendedDebug & {
-  web: debugThreadsNs.ExtendedDebug & {
-    thread: debugThreadsNs.ExtendedDebug;
-  }
-}
-
-export const debug: Debug = debugThreadsNs.setup('nats-micro');
