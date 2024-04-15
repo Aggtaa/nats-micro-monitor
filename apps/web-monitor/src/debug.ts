10dc81d116b9c94b8d6a6b94ef03e3035071b1de
Anton Zykov <a.zykov@optimacros.com>
2024-04-15 14:09:44 +0300

    web-monitor BE and FE moved to a separate repo

--- apps/web-monitor/src/debug.ts
+++
@@ -1,7 +0,0 @@
-import * as debugThreadsNs from 'debug-threads-ns';
-
-// eslint-disable-next-line @typescript-eslint/ban-types
-type Debug = debugThreadsNs.ExtendedDebug & {
-}
-
-export const debug: Debug = debugThreadsNs.setup('nats-micro');
